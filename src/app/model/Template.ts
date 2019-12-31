3dd11344fcad6f60af5e7313747f28951ffd84cf
patinvis <vishal.r.patil@tieto.com>
2019-12-31 14:55:50 +0530

    WFE changes

--- src/app/model/Template.ts
+++ src/app/model/Template.ts
@@ -1,76 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-export class JSON {
-  TemplateConfiguration: TemplateConfiguration;
-}
-export class TemplateConfiguration {
-  Template: Template[];
-}
-
 export class Template {
-  '-id': string;
-  '-name': string;
-  '-description'?: string;
-  CaseTimestampList?: CaseTimestampList;
-  CaseMethods?: CaseMethods;
-}
+  templateId: number;
+  templateName: string;
+  templateDesc: string;
 
-export class CaseMethods {
-  CaseEventOperationList?: CaseMethodsCaseEventOperationList;
-}
-
-export class CaseMethodsCaseEventOperationList {
-  CaseEventOperation: PurpleCaseEventOperation[] | PurpleCaseEventOperation;
-}
-
-export class PurpleCaseEventOperation {
-  id?: string;
-  OperationId: string;
-  ExecutionOrder?: string;
-  Parameters?: string;
-  EventName?: string;
-}
-
-export class CaseTimestampList {
-  CaseTimestamp: CaseTimestampElement[] | PurpleCaseTimestamp;
-}
-
-export class CaseTimestampElement {
-  TimestampId: string;
-  SortOrder: string;
-  EnableGUI?: string;
-  EnableClear?: string;
-  CaseEventOperationList?: PurpleCaseEventOperationList;
-  Hidden?: string;
-  TimestampGroupId?: string;
-  EnableSignatureChange?: string;
-  EnableBusinessLogic?: string;
-  EnableChangesAfterClosed?: string;
-  UpdateTimestampInNewTx?: string;
-  DependentTimestamp?: string;
-  RunOperationsInNewTx?: string;
-}
-
-export class PurpleCaseEventOperationList {
-  CaseEventOperation: FluffyCaseEventOperation[] | PurpleCaseEventOperation;
-}
-
-export class FluffyCaseEventOperation {
-  id: string;
-  OperationId: string;
-  EventName: string;
-  ExecutionOrder?: string;
-  Parameters?: string;
-  ErrorHandler?: string;
-  FailOnError?: string;
-}
-
-export class PurpleCaseTimestamp {
-  TimestampId: string;
-  SortOrder: string;
-  EnableGUI?: string;
-  CaseEventOperationList?: CaseMethodsCaseEventOperationList;
-  Hidden?: string;
-  EnableBusinessLogic?: string;
-  EnableChangesAfterClosed?: string;
+  constructor() { }
 }
